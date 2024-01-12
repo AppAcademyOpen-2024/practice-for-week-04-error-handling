c7bd8ebd2444471720edd42da02ac21fed329ab0
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-12 14:54:16 -0500

    complete

--- error-handling.js
+++ error-handling.js
@@ -7,14 +7,35 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.error(`Wrong Type: ${error.message}`);
+  }
+}
 
 // 2.
+function sayName(name) {
+  if (typeof name !== 'string') {
+    throw TypeError('Invalid name! Must be a string!');
+  } else {
+    console.log(name);
+  }
+}
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+try {
+  sayName("Alex");
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  sayName(1);
+} catch (error) {
+  console.error(error.message);
+}
 
 // 3.
 function greet(greeting) {
@@ -23,4 +44,11 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet();
+} catch (error) {
+  // console.error(error.message);
+  console.log('Hello World!');
 }
